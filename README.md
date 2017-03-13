e1b70ad88ccc681b2b684ef8913c3afa194ffd0c
Alexander Bigga <alexander.bigga@slub-dresden.de>
2017-03-13 14:48:03 +0100

    add basic files for extension

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ddb-frontend-viewer
+# DDB Frontend Viewer
+
+This TYPO3 extension provides the configuration and setup for the DDB Frontend Viewer.
+
+##  Installation
+This extension needs to reside in a folder called `ddb_frontend_viewer` in TYPO3 extension folder ('typoconf/ext'). 
+
+## Dependencies
+- TYPO3 CMS Frontend (cms)
+- CSS styled content (css_styled_content)
+- Extbase Framework (Extbase)
+- Fluid Templating Engine (fluid)
+- RealURL (realurl)
+- Kitodo.Presentatioin (dlf)
---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'DDB Frontend Viewer',
+	'description' => 'Viewer based on Kitodo.Presentation, customized for DDB',
+	'category' => 'templates',
+	'version' => '0.0.1',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Alexander Bigga',
+	'author_email' => 'alexander.bigga@slub-dresden.de',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '7.6.20-7.6.99',
+			'dlf' => '2.1.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
