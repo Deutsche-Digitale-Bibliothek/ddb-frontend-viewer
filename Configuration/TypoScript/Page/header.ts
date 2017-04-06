c09b64b133021b0103e88e5dd6ad7589882438ab
Thomas Jung <thomas@tjwd.de>
2017-04-06 19:53:32 +0200

    Add javascript file with inital tabbar function

--- Configuration/TypoScript/Page/header.ts
+++ Configuration/TypoScript/Page/header.ts
@@ -10,8 +10,9 @@ page {
 	}
 
 	includeJSlibs {
-    # we include jquery by t3jquery on page.9 below
-    plugins = EXT:ddb_frontend_viewer/Resources/Public/Js/plugins.js
-  }
+		# we include jquery by t3jquery on page.9 below
+		plugins = EXT:ddb_frontend_viewer/Resources/Public/Js/plugins.js
+		uiscripts = EXT:ddb_frontend_viewer/Resources/Public/Js/allScripts.js
+	}
 
 }
