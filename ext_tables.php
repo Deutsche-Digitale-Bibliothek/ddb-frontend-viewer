4478d57342949bc143438999e519be2e922e8b5f
Erik Sommer <Erik.Sommer@slub-dresden.de>
2017-04-13 13:11:19 +0200

    Add backend module to configure initial metadata and structure table

--- ext_tables.php
+++ ext_tables.php
@@ -4,3 +4,12 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'DDB Frontend Viewer');
+
+// Register modules.
+if (TYPO3_MODE == 'BE') {
+
+    // Module "setup".
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('txdlfmodules', 'txddbfrontendviewersetup', '', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'modules/setup/');
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdlfmodules_txddbfrontendviewersetup', 'EXT:ddb_frontend_viewer/modules/setup/locallang_mod.xml');
+}
