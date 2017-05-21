29e1a7ee104f8225d31439b4e1e3b57e9bf39dd8
Alexander Bigga <alexander.bigga@slub-dresden.de>
2017-05-21 22:22:35 +0200

    set content-security-policy meta tag

--- Configuration/TypoScript/Page/header.ts
+++ Configuration/TypoScript/Page/header.ts
@@ -19,6 +19,8 @@ page {
 		author.field = author
 		robots = all
 		viewport = width=device-width, initial-scale=1
+		Content-Security-Policy = upgrade-insecure-requests
+		Content-Security-Policy.httpEquivalent = 1
 	}
 
 }
