b588c2f436ca42319ecda145b8efd942534e959b
Alexander Bigga <alexander.bigga@slub-dresden.de>
2017-05-22 10:59:15 +0200

    we cannot set CSP upgrade-insecure-requests as not all providers support clean HTTPS .-(

--- Configuration/TypoScript/Page/header.ts
+++ Configuration/TypoScript/Page/header.ts
@@ -19,8 +19,9 @@ page {
 		author.field = author
 		robots = all
 		viewport = width=device-width, initial-scale=1
-		Content-Security-Policy = upgrade-insecure-requests
-		Content-Security-Policy.httpEquivalent = 1
+		# force
+		# Content-Security-Policy = upgrade-insecure-requests
+		# Content-Security-Policy.httpEquivalent = 1
 	}
 
 }
